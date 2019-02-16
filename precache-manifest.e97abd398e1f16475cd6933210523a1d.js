1bc95412f66d5e34f5bc7f48d1e48ed70efdbd26
Robert Bossaert <contact@robertbossaert.com>
2019-02-16 16:09:41 +0100

    Updates

---
+++ precache-manifest.e97abd398e1f16475cd6933210523a1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "311a680ae87708376bdb83c36760b353",
+    "url": "/react-pouchdb-couchdb-demo/static/media/logo.311a680a.png"
+  },
+  {
+    "revision": "6e2bba6f5d162bd6696c",
+    "url": "/react-pouchdb-couchdb-demo/static/js/runtime~main.6e2bba6f.js"
+  },
+  {
+    "revision": "02bf1b95fdfe5cc770ea",
+    "url": "/react-pouchdb-couchdb-demo/static/js/main.02bf1b95.chunk.js"
+  },
+  {
+    "revision": "1e7b67d63e9a3ba125bc",
+    "url": "/react-pouchdb-couchdb-demo/static/js/2.1e7b67d6.chunk.js"
+  },
+  {
+    "revision": "c3e8db8be79bc738485dc2d1eda5f4c3",
+    "url": "/react-pouchdb-couchdb-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6987cbc9ec0b01ff374a74944c51fc22.js"
+  "/react-pouchdb-couchdb-demo/precache-manifest.e97abd398e1f16475cd6933210523a1d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-pouchdb-couchdb-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
